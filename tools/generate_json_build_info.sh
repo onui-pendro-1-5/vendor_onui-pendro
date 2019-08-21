e899881b00eb4dfa1dbbf350ae9ab6ecf76ed7e2
Raghu Varma <raghuvarma331@gmail.com>
2019-08-21 10:38:28 +0530

    json: get back to sourfeforge
    As per the users request we moved to sourceforge

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,7 +3,7 @@ if [ "$1" ]
 then
   datetime=$(grep org\.onui-pendro\.build_date_utc $OUT/system/build.prop | cut -d= -f2)
   romtype=$CUSTOM_BUILD_TYPE\
-  url=https://gitlab.com/onui-pendro-1.5_downloads/$CUSTOM_BUILD/raw/master/$(basename $(ls $OUT/OnUI-Pendro*.zip))\
+  url=https://sourceforge.net/projects/onui-pendro/files/$CUSTOM_BUILD/$(basename $(ls $OUT/OnUI-Pendro*.zip))\
   version=$(grep -m1 org\.onui-pendro\.version $OUT/system/build.prop | cut -d= -f2)\
   file_path=$1
   filename=$(basename "$file_path")
@@ -12,3 +12,4 @@ then
     echo "{\n\"response\": [\n{\n   \"datetime\": \"$datetime\",\n   \"filename\": \"$filename\",\n   \"id\": \"$id\",\n   \"romtype\": \"$romtype\",\n   \"size\": $(stat -c%s $OUT/OnUI-Pendro*.zip),\n   \"url\": \"$url\",\n   \"version\": \"$version\"\n}\n]\n}" > $file_path.json	
   fi
 fi
+
